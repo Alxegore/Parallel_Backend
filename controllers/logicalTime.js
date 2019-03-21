189553d654f1d5128831e4b02ec59ec540aef836
jumpwmk <methus.kiat@gmail.com>
2019-03-21 08:40:08 +0700

    change getLogicalTime

--- controllers/logicalTime.js
+++ controllers/logicalTime.js
@@ -1,15 +1,26 @@
+// var express = require('express');
+// const app = express()
+// const cors = require('cors')
+// app.use(cors())
 
-async function updateLogicalTime(){
-    await logicalTime.updateOne({logicalTime: globalLogicalTime});
-}
+// var globalLogicalTime = 0;
 
-async function getLogicalTime(){
-    var query = await logicalTime.findOne()
-    globalLogicalTime = query.logicalTime + 1;
-    await updateLogicalTime()
-}
+// const logicalTime = require('../models/logicalTime');
+// // Set up mongoose connection
+// var mongoose = require('mongoose');
+// const dev_db_url = 'mongodb+srv://admineq:admineq@parallel-fnvjs.mongodb.net/test?retryWrites=true';
+// // var mongoDB = process.env.MONGODB_URI || dev_db_url;
+// mongoose.connect(dev_db_url);
+// mongoose.Promise = global.Promise;
 
+// async function getLogicalTime(){
+//     var query = await logicalTime.findOneAndUpdate(
+//         {}, 
+//         { 
+//            $inc: { logicalTime: 1 } 
+//         }, {new: true })
+//     globalLogicalTime = query.logicalTime
+//     console.log(globalLogicalTime)
+// }
 
-(async () => {
-    await getLogicalTime()
-})()
+// getLogicalTime()
